24d2149263a4524bbd80e10b8177ea8b5d96ac17
Nikita <zeblol68@gmail.com>
2026-06-01 03:17:29 +0300

    nixpacksadd

---
+++ .nixpacks/hooks/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+mkdir -p /usr/local/etc/php/conf.d || true
+install -m644 .nixpacks/conf.d/custom.ini /usr/local/etc/php/conf.d/custom.ini || cp .nixpacks/conf.d/custom.ini /tmp/custom.ini || true
