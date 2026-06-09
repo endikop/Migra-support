185dfac13b3daa1726cba16308b5fd0db8c8df2f
Nikita <zeblol68@gmail.com>
2026-06-09 03:59:00 +0300

    metayandex

--- index.php
+++ index.php
@@ -1017,6 +1017,7 @@ $translations = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="ebe4b7cb6371cb03" />
     <title><?php echo $translations['main_title']; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
